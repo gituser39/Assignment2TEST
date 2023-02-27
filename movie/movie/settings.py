a6464ddcadba92144557bdecc607a893572c3bad
gituser39 <42620763+gituser39@users.noreply.github.com>
2023-02-27 18:52:12 -0500

    * replace

--- movie/movie/settings.py
+++ movie/movie/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x8%d_f3&@_zry(s_0h4bjjctvmsuo%kc&ybpdaxa%(#(pi&^$v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
